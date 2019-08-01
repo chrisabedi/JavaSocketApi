becb2a7f04305e852fba2ef6683e28bfc028d141
Chris Abedi <cabedi@vertafore.com>
2019-08-01 13:30:03 -0600

    added SocketWorker and ClientWorker implementations

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -4,32 +4,53 @@ import java.io.*;
 
 public class Client {
 
-    public static void main (String [] args){
-        String serverName = args[0];
-        int port = Integer.parseInt(args[1]);
-        try {
-            System.out.println(String.format("Connecting to Server %s} on Port %d",serverName,port));
-           Socket client = new Socket(serverName, port);
+    public static class ClientWorker implements Runnable {
 
-           OutputStream outToServer = client.getOutputStream();
-           DataOutput out = new DataOutputStream(outToServer);
+        String serverName;
+        int port;
+        Socket socket;
 
-           out.writeUTF("Hello from " + client.getLocalSocketAddress());
-           InputStream inFromServer = client.getInputStream();
-           DataInput in = new DataInputStream(inFromServer);
+        ClientWorker(String serverName, int port) {
+            this.serverName = serverName;
+            this.port = port;
+            System.out.println(String.format("Connecting to Server %s on Port %d", serverName, port));
+            try {
+                this.socket = new Socket(serverName, port);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
 
-           System.out.println("Server responds with " + in.readUTF());
-           client.close();
+        }
 
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        @Override
+        public void run() {
+            try {
+                OutputStream outToServer = socket.getOutputStream();
+                DataOutput out = new DataOutputStream(outToServer);
+
+                out.writeUTF("Hello from " + socket.getLocalSocketAddress());
+                InputStream inFromServer = socket.getInputStream();
+                DataInput in = new DataInputStream(inFromServer);
+
+                System.out.println("Server responds with " + in.readUTF());
+                socket.close();
+
+            } catch ( IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
 
+    public static void main(String[] args) {
+        String serverName = (args[0]);
+        int port = Integer.parseInt(args[1]);
+        for (int i = 0; i < 10; i++) {
+            ClientWorker clientWorker = new ClientWorker(serverName, port);
+            Thread t = new Thread(clientWorker);
+            t.start();
+        }
 
-
-
+    }
 }
+
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -4,27 +4,50 @@ import java.io.*;
 
 public class Server implements Runnable{
 
+    public class SocketWorker implements Runnable {
+
+        Socket socket;
+        public SocketWorker(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+
+                DataInputStream in = new DataInputStream(socket.getInputStream());
+                System.out.println("waiting for client on Port " + serverSocket.getLocalPort() + "... ");
+
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                out.writeUTF("Thanks for connecting to server.java at " + socket.getLocalSocketAddress() + "\n !!!!");
+
+                //socket.close();
+            }
+            catch (IOException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
     private ServerSocket serverSocket;
+
     public Server(int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(100000);
+        //serverSocket.setSoTimeout(100000);
     }
 
     @Override
     public void run() {
         while(true) {
             try {
-                System.out.println("waiting for client on Port " + serverSocket.getLocalPort() + "... ");
                 Socket s = serverSocket.accept();
-                DataInputStream in = new DataInputStream(s.getInputStream());
-
-                DataOutputStream out = new DataOutputStream(s.getOutputStream());
-                out.writeUTF("Thanks for connecting to server.java at " + s.getLocalSocketAddress() + "\n !!!!");
-
-                s.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
+                SocketWorker newSock = new SocketWorker(s);
+                Thread t = new Thread(newSock);
+                t.start();
+            }
+            catch(IOException ex){
+                ex.printStackTrace();
             }
         }
 
